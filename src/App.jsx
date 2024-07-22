41ae848771eba7857d4b8095249fa983a8c62eb3
gehlotnikhil <111073969+gehlotnikhil@users.noreply.github.com>
2024-07-22 14:33:32 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [latestMessage, setLatestMessage] = useState("");
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    const newsocket = new WebSocket("ws://localhost:8080");
+    newsocket.onopen = () => {
+      console.log("Connected");
+      setSocket(newsocket);
+    };
+    newsocket.onmessage = (message) => {
+      console.log("Message received:", message.data);
+      setLatestMessage(message.data);
+    };
+    console.log("socket = ",newsocket);
+
+    return () => {
+      // when component is unmounted and new one will make
+      newsocket.close(); // we simply close the ws connection.
+    };
+  }, []);
+  useEffect(() => {
+    console.log(latestMessage);
+  }, [latestMessage]);
+
+  if (!socket) {
+    return (
+      <>
+        <div>Connecting to the socket connection. Loading...</div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h1>hello</h1>
+      <input
+        type="text"
+        name=""
+        id=""
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          socket.send(message);
+        }}
+      >
+        Submit
+      </button>
+      hi- {latestMessage}
+    </>
+  );
+}
+
+export default App;
